9a8955cf9dbd39c783cb6d7dc67066cbf2904fb4
aleksey <Aleksey.fgl@gmail.com>
2023-01-08 12:17:33 +0300

    del unused code

--- src/App.tsx
+++ src/App.tsx
@@ -11,26 +11,7 @@ type  TasksType = {
 }
 
 function App() {
-    const tasks1 = [
-        {id: 1, title: "HTML&CSS", isDone: true},
-        {id: 2, title: "JS", isDone: true},
-        {id: 3, title: "ReactJS", isDone: false}
-    ]
-    const tasks2 = [
-        {id: 1, title: "Hello world", isDone: true},
-        {id: 2, title: "I am Happy", isDone: false},
-        {id: 3, title: "Yo", isDone: false},
-        {id: 4, title: "Yo", isDone: false}
-    ]
 
-
-    const title: string = 'First Todolist'
-
-    function removeTask(id: number) {
-
-        let result = tasks2.filter((t: TasksType) => id !== t.id)
-        console.log(result)
-    }
 
     const btn1Foo = (user: string, age: number) => {
         console.log(user, age)
@@ -44,11 +25,6 @@ function App() {
 
     return (
         <div className="App">
-            {/*<Todolist title={title}*/}
-            {/*          tasks={tasks1}/>*/}
-            {/*<Todolist tasks={tasks2}*/}
-            {/*          removeTask={removeTask}/>*/}
-
             <Button name={'btn-1'} callBack={() => btn1Foo('Alex', 21)}/>
             <Button name={'btn-2'} callBack={() => btn2Foo('Vasya', 22)}/>
             <Button name={'stupid button'} callBack={btn3Foo}/>
